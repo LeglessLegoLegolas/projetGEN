0cfda01a279ea81752afcc06c3ebb730f2728d49
Vermi <romain.castanie31@gmail.com>
2014-07-01 16:03:18 +0200

    update

--- SolutioProjetGEN/GUI/Authentification.Designer.cs
+++ SolutioProjetGEN/GUI/Authentification.Designer.cs
@@ -97,7 +97,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::GUI.Properties.Resources.aut2;
             this.ClientSize = new System.Drawing.Size(280, 300);
             this.Controls.Add(this.labelInformation);
             this.Controls.Add(this.labelPassword);
