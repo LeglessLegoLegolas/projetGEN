ab1f3480f261f473e56252a806350857733217c2
Vermi <romain.castanie31@gmail.com>
2014-07-01 16:08:59 +0200

    no more problem

--- SolutioProjetGEN/GUI/Authentification.Designer.cs
+++ SolutioProjetGEN/GUI/Authentification.Designer.cs
@@ -97,6 +97,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GUI.Properties.Resources.backgroundAuth;
             this.ClientSize = new System.Drawing.Size(280, 300);
             this.Controls.Add(this.labelInformation);
             this.Controls.Add(this.labelPassword);
