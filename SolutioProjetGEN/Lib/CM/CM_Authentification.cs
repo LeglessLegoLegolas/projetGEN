8bdfd3afe6b18531afd5661b8278663445fa63e3
Vermi <romain.castanie31@gmail.com>
2014-07-09 16:30:40 +0200

    Ajout BDD utilisateurs

---
+++ SolutioProjetGEN/Lib/CM/CM_Authentification.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFDataContracts;
+
+namespace Lib
+{
+    public class Auth
+
+    {
+        private static List<string> listLogin = Lib.EM_Utilisateur.mLogin();
+        private static List<string> listPassword = Lib.EM_Utilisateur.mPassword();
+
+        public static Stg Authentification(Stg msg)
+        {
+            int i = 0;
+            foreach (string s in listLogin)
+            {
+                if (msg._data[0].ToString() == listLogin[i] && msg._data[1].ToString() == listPassword[i])
+                {
+                    msg._statut_op = true;
+                    break;
+                }
+                else
+                {
+                    msg._statut_op = false;
+                }
+                ++i;
+            }
+            return msg;
+        }
+    }
---
+++ SolutioProjetGEN/Lib/EM/EM_Utilisateur.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lib
+{
+    class EM_Utilisateur
+    {
+        public static List<string> mLogin()
+        {
+            List<string> myList = new List<string>();
+
+            var data = new generatorDatabaseDataSetTableAdapters.UserTableAdapter();
+            foreach (var dr in data.GetData())
+            {
+                myList.Add(dr["Login"].ToString());
+            }
+            return myList;
+        }
+
+        public static List<string> mPassword()
+        {
+            List<string> myList2 = new List<string>();
+
+            var data = new generatorDatabaseDataSetTableAdapters.UserTableAdapter();
+            foreach (var dr in data.GetData())
+            {
+                myList2.Add(dr["Password"].ToString());
+            }
+            return myList2;
+        }
+    }
+}
