bad35d03b401cc02559f5f033ad1329c1d9460cf
Vermi <romain.castanie31@gmail.com>
2014-07-09 16:51:26 +0200

    Update CU_Auth

--- SolutioProjetGEN/ClientSOAP/CU/Authentification.Designer.cs
+++ SolutioProjetGEN/ClientSOAP/CU/Authentification.Designer.cs
@@ -101,7 +101,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(283, 305);
+            this.ClientSize = new System.Drawing.Size(284, 301);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -110,6 +110,8 @@
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(300, 340);
+            this.MinimumSize = new System.Drawing.Size(300, 340);
             this.Name = "Authentification";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Authentification";
